654b652ef83f2b1cc353171916dfd6bc8a9c0338
d4mie <damio.ogunnaike@gmail.com>
2025-12-15 00:32:45 +0100

    removed divider

--- components/ProductGrid.js
+++ components/ProductGrid.js
@@ -5,7 +5,7 @@ export default function ProductGrid({ products = [] }) {
   return (
     <section
       id="products"
-      className="relative overflow-hidden border-y border-amber-400 bg-black py-16 text-white"
+      className="relative overflow-hidden border-t border-amber-400 bg-black py-16 text-white"
     >
       <Watermark />
       <div className="mx-auto grid max-w-6xl grid-cols-1 gap-6 px-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5">
